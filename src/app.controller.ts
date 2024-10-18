5d189a08676255a492ffa97d85094442656b3448
Surafel Workayehu <surafelwork32@gmail.com>
2024-10-18 15:12:44 +0300

    Refactor authentication: replace dynamic secret retrieval with static token and remove unused guard factory

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,15 @@
 import { Body, Controller, Headers, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { secretTokenGuard } from './auth/guard.factory';
+import { AuthGuard } from './auth/auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post('/hello')
-  // @UseGuards(AuthGuard)
-  @UseGuards(secretTokenGuard('aladia-100ms-header', 'salakute'))
+  @UseGuards(AuthGuard)
   getHello(@Body() body: any, @Headers('authorization') auth: string): string {
-    console.log('Received a webhook from Aladia');
+    console.log('Received a webhook event from Aladia');
     console.log(body, 'body');
     console.log(auth, 'auth');
     return this.appService.getHello();
--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -7,8 +7,7 @@ import {
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  private readonly staticToken =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NmZiYjFiMTZmYzY0NTVhNGRkMjBlZjkiLCJpYXQiOjE3Mjc4NzA4NzcsImV4cCI6MTcyNzg3NDQ3NywiYXVkIjoibGNhbGhvc3Q6MzAwMCIsImlzcyI6ImxvY2FsaG9zdDozMDAwIn0.rpc9uHATsl9zt3DIbb8gHOnENzarFfLUebSmPo9Se1c';
+  private readonly staticToken = 'test-secret';
 
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
